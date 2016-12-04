17f6e9dd90e3b767694e378226ac5d2e3a321e45
brahma <brahma@gopassport.com>
2016-12-04 14:12:19 +0530

    initial commit

---
+++ On The Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  On The Map
+//
+//  Created by Brahma Reddy Chilakala on 04/12/16.
+//  Copyright © 2016 Brahma Reddy Chilakala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
